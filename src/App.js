7775d4264efc2a6f183f9ede82ed9ef5799aa1ea
Connor Southwell <south1cr@gmail.com>
2023-05-09 17:34:49 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import{ useState } from 'react';
+import contacts from "./contacts.json";
+
+
+
+const contactsArr = contacts.slice(0, 5);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [contactsArr, setContacts] = useState(contacts.slice(0, 5));
+  
+  const addContact = () => {
+    let contact = contacts[Math.floor(Math.random() * contacts.length)]
+    setContacts( arr => [...arr, contact]);
+  }
+
+  const sortName = () => {
+    function compare( a, b ) {
+      if ( a.name < b.name ){
+        return -1;
+      }
+      if ( a.name > b.name ){
+        return 1;
+      }
+      return 0;
+    }
+    let arr = [...contactsArr];
+    arr.sort(compare)
+    setContacts( e => [...arr]);
+  }
+
+  const sortPopularity = () => {
+    function compare( a, b ) {
+      if ( a.popularity < b.popularity ){
+        return -1;
+      }
+      if ( a.popularity > b.popularity ){
+        return 1;
+      }
+      return 0;
+    }
+    let arr = [...contactsArr];
+    arr.sort(compare)
+    setContacts( e => [...arr]);
+  }
+
+  const deleteContact = (id) => {
+    let contactArray = [...contactsArr];
+    contactArray.forEach((elem, i, arr) => {
+      if(id === elem.id){
+        arr.splice(i, 1)
+      }
+    })
+    setContacts( e => [...contactArray]);
+  }
+
+  return <div className="App">
+    <h1>IronContacts</h1>
+    <button onClick={addContact}>Add Contact</button>
+    <button onClick={sortName}>Sort Name</button>
+    <button onClick={sortPopularity}>Sort Popularity</button>
+    <table>
+      <tbody>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+          <th></th>
+        </tr>
+        {contactsArr.map((contact, i) => {
+          return <tr key={i}>
+            <td><img src={contact.pictureUrl}></img></td>
+            <td>{contact.name}</td>
+            <td>{contact.popularity}</td>
+            <td>{contact.wonOscar && <img src="https://github.githubassets.com/images/icons/emoji/unicode/1f3c6.png"></img>}</td>
+            <td>{contact.wonEmmy && <img src="https://github.githubassets.com/images/icons/emoji/unicode/1f3c6.png"></img>}</td>
+            <td><button onClick={() => {deleteContact(contact.id)}}>Delete</button></td>
+          </tr>
+        })}
+      </tbody>
+    </table>
+  </div>;
+}
 export default App;
